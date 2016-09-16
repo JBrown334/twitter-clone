6375989a65430228b1d60ebd42e0d6a5acb5ad2f
Joe Brown <joebrown28@gmail.com>
2016-09-15 19:13:41 -0600

    FOR GUNNA

---
+++ js/animations.js
@@ -0,0 +1,16 @@
+console.log('hello');
+var hello = "hello";
+$(document).ready(function(){
+  // var $=jQuery;
+//Initially, the Tweet button and the character count button should be hidden (CSS).
+$('#tweet-controls').hide();
+
+
+
+
+
+
+
+
+
+});
